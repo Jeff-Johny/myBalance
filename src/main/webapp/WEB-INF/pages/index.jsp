bce0f35087a8381758ed8f7aa74160c89c90bc3f
Rahul nair <rahuln@qburst.com>
2015-10-26 20:07:52 +0530

    first version

--- src/main/webapp/WEB-INF/pages/index.jsp
+++ src/main/webapp/WEB-INF/pages/index.jsp
@@ -2,20 +2,43 @@
 <!DOCTYPE html>
 <html>
 	<head>
+		<link rel="stylesheet" href="<c:url value="/resources/styles/pure-min.css" />" type="text/css" media="screen" />
 		<link rel="stylesheet" href="<c:url value="/resources/styles/myStyle.css" />" type="text/css" media="screen" />
 		<script type="text/javascript" src="<c:url value="/resources/lib/resources/angular.min.js" />"></script>
 		<script type="text/javascript" src="<c:url value="/resources/items/app.js" />"></script>
 		<script type="text/javascript" src="<c:url value="/resources/items/controllers/tableController.js" />"></script>
 		<script type="text/javascript" src="<c:url value="/resources/items/services/tableService.js" />"></script>
 	</head>
-	<body ng-app="myApp">
-		<div ng-controller="tableController" ng-init="init()">
-			<table>
-				<tr ng-repeat="user in users">
-					<td>{{user.name}}</td>
-					<td>{{user.balance}}</td>
-				</tr>
-			</table>
+	<body ng-app="myApp" ng-controller="tableController" ng-init="init()">
+		<div class="pure-g">
+			<div class="pure-u-2-3">
+				<table class="pure-table">
+					<tr ng-repeat="order in orders">
+						<td>{{order.name}}</td>
+						<td>
+							<input type="radio" ng-model="order.isShare" value="share" ng-change="updateAmount()"/> Share
+							<input type="radio" ng-model="order.isShare" value="full" ng-change="updateAmount()"/> Full
+						</td>
+						<td>
+							<input type="radio" ng-model="order.item" value="veg" ng-change="updateAmount()"/> Veg
+							<input type="radio" ng-model="order.item" value="chicken" ng-change="updateAmount()"/> Chicken
+							<input type="radio" ng-model="order.item" value="beef" ng-change="updateAmount()"/> Beef
+							<input type="radio" ng-model="order.item" value="fish" ng-change="updateAmount()"/> Fish
+						</td>
+						<td>
+							{{order.amount}}
+						</td>
+					</tr>
+				</table>
+			</div>
+			<div class="pure-u-1-3">
+				<table class="pure-table">
+					<tr ng-repeat="user in users">
+						<td>{{user.name}}</td>
+						<td>{{user.balance}}</td>
+					</tr>
+				</table>
+			</div>
 		</div>
 	</body>
 </html>
--- src/main/webapp/items/controllers/tableController.js
+++ src/main/webapp/items/controllers/tableController.js
@@ -3,15 +3,61 @@ app.controller('tableController', function($scope, tableService) {
 	
     $scope.users = [];
     $scope.init  = function() {
-    	$scope.users = [{name:'J',balance:23},
-    	                {name:'L',balance:36},
-    	                {name:'R',balance:13},
-    	                {name:'M',balance:76}
-        ]
+    	$scope.users = [];
+        $scope.date = $scope.getCurrentDate();
+        $scope.orders = [{
+            name: 'Jeff',
+            isShare: 'share',
+            item: 'veg',
+            amount: 20
+        },{
+            name: 'Nithin',
+            isShare: 'share',
+            item: 'veg',
+            amount: 20
+        }];
     	tableService.getDetails($scope);
 	};
-    $scope.clear = function() {
-    	
+    $scope.getCurrentDate = function(){
+        var today = new Date();
+        var dd = today.getDate();
+        var mm = today.getMonth()+1; //January is 0!
+
+        var yyyy = today.getFullYear();
+        if(dd<10){
+            dd='0'+dd
+        } 
+        if(mm<10){
+            mm='0'+mm
+        } 
+        var today = dd+'/'+mm+'/'+yyyy;
+        return today;
+    }
+    $scope.updateAmount = function() {
+    	var shares = $scope.orders.filter(function(t,i){return t.isShare ==="share"}).length;
+        var beefAmt = 40, chickenAmt = 40, fishAmt = 30, mealAmt = 0, specialAmt = 0;
+
+        switch(shares){
+            case 1: $scope.sharedMeals = 1; break;
+            case 2: $scope.sharedMeals = 1; break;
+            case 3: $scope.sharedMeals = 2; break;
+            case 4: $scope.sharedMeals = 3; break;
+            default : $scope.sharedMeals = shares - 2;
+        }
+        mealAmt = ($scope.sharedMeals * 40) / shares;
+        $scope.orders.forEach(function(item, index){
+            switch(item.item){
+                case 'beef': specialAmt = beefAmt; break;
+                case 'chicken': specialAmt = chickenAmt; break;
+                case 'fish': specialAmt = fishAmt; break;
+                default : specialAmt = 0;
+            }
+            if(item.isShare === "full"){
+                item.amount = 40 + specialAmt;
+            }else{
+                item.amount = mealAmt + specialAmt;
+            }
+        });
     };
     $scope.save  = function() {
     	
